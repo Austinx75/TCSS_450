0e84e632246702fb5d356e0e66a8b611dde5ff5e
Austin Scott <austin.l.scott75@gmail.com>
2021-04-26 16:52:18 -0700

    Login / Registration Functionality

--- Harmony/app/build/generated/data_binding_base_class_source_out/debug/out/edu/uw/harmony/databinding/ActivityAuthBinding.java
+++ Harmony/app/build/generated/data_binding_base_class_source_out/debug/out/edu/uw/harmony/databinding/ActivityAuthBinding.java
@@ -4,7 +4,6 @@ package edu.uw.harmony.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -12,23 +11,18 @@ import androidx.viewbinding.ViewBinding;
 import edu.uw.harmony.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
-import java.lang.String;
 
 public final class ActivityAuthBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
   @NonNull
-  public final ConstraintLayout boxInsetLayout;
-
-  @NonNull
-  public final FrameLayout frameLayout;
+  public final ConstraintLayout frameMainFragmentContainer;
 
   private ActivityAuthBinding(@NonNull ConstraintLayout rootView,
-      @NonNull ConstraintLayout boxInsetLayout, @NonNull FrameLayout frameLayout) {
+      @NonNull ConstraintLayout frameMainFragmentContainer) {
     this.rootView = rootView;
-    this.boxInsetLayout = boxInsetLayout;
-    this.frameLayout = frameLayout;
+    this.frameMainFragmentContainer = frameMainFragmentContainer;
   }
 
   @Override
@@ -54,21 +48,12 @@ public final class ActivityAuthBinding implements ViewBinding {
 
   @NonNull
   public static ActivityAuthBinding bind(@NonNull View rootView) {
-    // The body of this method is generated in a way you would not otherwise write.
-    // This is done to optimize the compiled bytecode for size and performance.
-    int id;
-    missingId: {
-      ConstraintLayout boxInsetLayout = (ConstraintLayout) rootView;
-
-      id = R.id.frameLayout;
-      FrameLayout frameLayout = rootView.findViewById(id);
-      if (frameLayout == null) {
-        break missingId;
-      }
-
-      return new ActivityAuthBinding((ConstraintLayout) rootView, boxInsetLayout, frameLayout);
+    if (rootView == null) {
+      throw new NullPointerException("rootView");
     }
-    String missingId = rootView.getResources().getResourceName(id);
-    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
+
+    ConstraintLayout frameMainFragmentContainer = (ConstraintLayout) rootView;
+
+    return new ActivityAuthBinding((ConstraintLayout) rootView, frameMainFragmentContainer);
   }
 }
--- Harmony/app/src/main/java/edu/uw/harmony/UI/Auth/LogIn/LogInFragment.java
+++ Harmony/app/src/main/java/edu/uw/harmony/UI/Auth/LogIn/LogInFragment.java
@@ -2,65 +2,172 @@ package edu.uw.harmony.UI.Auth.LogIn;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import edu.uw.harmony.R;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.uw.harmony.databinding.FragmentLogInBinding;
+
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LogInFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class LogInFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentLogInBinding binding;
+    private LogInViewModel mSignInModel;
 
     public LogInFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LogInFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LogInFragment newInstance(String param1, String param2) {
-        LogInFragment fragment = new LogInFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSignInModel = new ViewModelProvider(getActivity())
+                .get(LogInViewModel.class);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentLogInBinding.inflate(inflater);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonLoginFragmentRegister.setOnClickListener(button ->
+                Navigation.findNavController(getView()).navigate(
+                        LogInFragmentDirections.actionLogInFragmentToRegisterFragment()
+                ));
+
+        binding.buttonLoginFragmentLogin.setOnClickListener(button->Verify(view));
+
+
+        mSignInModel.addResponseObserver(
+                getViewLifecycleOwner(),
+                this::observeResponse);
+
+
+        LogInFragmentArgs args = LogInFragmentArgs.fromBundle(getArguments());
+        binding.editTextEmail.setText(args.getEmail().equals("default") ? "" : args.getEmail());
+        binding.editTextPassword.setText(args.getPassword().equals("default") ? "" : args.getPassword());
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void Verify(View view){
+        if(binding.editTextEmail.getText().toString().isEmpty()) {
+            binding.editTextEmail.setError("Empty Email");
+        } else if(binding.editTextPassword.getText().toString().isEmpty()){
+            binding.editTextPassword.setError("Empty Password");
+        }
+
+        verifyAuthWithServer();
+
+    }
+
+    /**
+     * This helper method is creating a JSON Web Token (JWT). In future labs, the JWT will
+     * be created and sent to us from the Web Service. For now, we will "fake" that and create
+     * the JWT client-side. This is ANTI-PATTERN!!! Do not create JWTs client-side.
+     *
+     * @param email the email used to encode into the JWT
+     * @return the resulting JWT
+     */
+    private String generateJwt(final String email) {
+        String token;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret key don't use a string literal in " +
+                    "production code!!!");
+            token = JWT.create()
+                    .withIssuer("auth0")
+                    .withClaim("email", email)
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("JWT Failed to Create.");
+        }
+        return token;
+    }
+
+    private void verifyAuthWithServer() {
+        mSignInModel.connect(
+                binding.editTextEmail.getText().toString(),
+                binding.editTextPassword.getText().toString());
+
+        //This is an Asynchronous call. No statements after should rely on the result of connect.
+
+    }
+
+    /**
+     * Helper to abstract the navigation to the Activity past Authentication.
+     * @param email users email
+     * @param jwt the JSON Web Token supplied by the server
+     */
+    private void navigateToSuccess(final String email, final String jwt) {
+        Navigation.findNavController(getView())
+                .navigate(LogInFragmentDirections
+                        .actionLogInFragmentToMainActivity(jwt, email));
+
+        getActivity().finish();
+    }
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    binding.editTextEmail.setError(
+                            "Error Authenticating: " +
+                                    response.getJSONObject("data").getString("message"));
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+
+                try {
+
+                    navigateToSuccess(
+                            binding.editTextEmail.getText().toString(), response.getString("token")
+                    );
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
+
+    }
+
 }
