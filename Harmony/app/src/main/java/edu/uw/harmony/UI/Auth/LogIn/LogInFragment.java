75870265cba096981c42f16648825c67cd0bdb6c
Austin Scott <austin.l.scott75@gmail.com>
2021-04-28 15:43:32 -0700

    New Version of LogIn / Registration

--- Harmony/app/src/main/java/edu/uw/harmony/UI/Auth/LogIn/LogInFragment.java
+++ Harmony/app/src/main/java/edu/uw/harmony/UI/Auth/LogIn/LogInFragment.java
@@ -8,19 +8,28 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import edu.uw.harmony.databinding.FragmentLogInBinding;
+import edu.uw.harmony.uitil.PasswordValidator;
+
+import static edu.uw.harmony.uitil.PasswordValidator.checkExcludeWhiteSpace;
+import static edu.uw.harmony.uitil.PasswordValidator.checkPwdLength;
+import static edu.uw.harmony.uitil.PasswordValidator.checkPwdSpecialChar;
 
 
 /**
@@ -30,6 +39,13 @@ public class LogInFragment extends Fragment {
 
     private FragmentLogInBinding binding;
     private LogInViewModel mSignInModel;
+
+    private PasswordValidator mEmailValidator = checkPwdLength(2)
+            .and(checkExcludeWhiteSpace())
+            .and(checkPwdSpecialChar("@"));
+
+    private PasswordValidator mPassWordValidator = checkPwdLength(1)
+            .and(checkExcludeWhiteSpace());
 
     public LogInFragment() {
         // Required empty public constructor
@@ -42,9 +58,6 @@ public class LogInFragment extends Fragment {
                 .get(LogInViewModel.class);
     }
 
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -56,71 +69,51 @@ public class LogInFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         binding.buttonLoginFragmentRegister.setOnClickListener(button ->
                 Navigation.findNavController(getView()).navigate(
                         LogInFragmentDirections.actionLogInFragmentToRegisterFragment()
                 ));
 
-        binding.buttonLoginFragmentLogin.setOnClickListener(button->Verify(view));
-
+        binding.buttonLoginFragmentLogin.setOnClickListener(this::attemptSignIn);
 
         mSignInModel.addResponseObserver(
                 getViewLifecycleOwner(),
                 this::observeResponse);
 
-
         LogInFragmentArgs args = LogInFragmentArgs.fromBundle(getArguments());
         binding.editTextEmail.setText(args.getEmail().equals("default") ? "" : args.getEmail());
         binding.editTextPassword.setText(args.getPassword().equals("default") ? "" : args.getPassword());
-
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void attemptSignIn(final View button) {
+        validateEmail();
     }
 
-    private void Verify(View view){
-        if(binding.editTextEmail.getText().toString().isEmpty()) {
-            binding.editTextEmail.setError("Empty Email");
-        } else if(binding.editTextPassword.getText().toString().isEmpty()){
-            binding.editTextPassword.setError("Empty Password");
-        }
-
-        verifyAuthWithServer();
-
+    private void validateEmail() {
+        mEmailValidator.processResult(
+                mEmailValidator.apply(binding.editTextEmail.getText().toString().trim()),
+                this::validatePassword,
+                result -> binding.editTextEmail.setError("Please enter a valid Email address."));
     }
 
-    /**
-     * This helper method is creating a JSON Web Token (JWT). In future labs, the JWT will
-     * be created and sent to us from the Web Service. For now, we will "fake" that and create
-     * the JWT client-side. This is ANTI-PATTERN!!! Do not create JWTs client-side.
-     *
-     * @param email the email used to encode into the JWT
-     * @return the resulting JWT
-     */
-    private String generateJwt(final String email) {
-        String token;
-        try {
-            Algorithm algorithm = Algorithm.HMAC256("secret key don't use a string literal in " +
-                    "production code!!!");
-            token = JWT.create()
-                    .withIssuer("auth0")
-                    .withClaim("email", email)
-                    .sign(algorithm);
-        } catch (JWTCreationException exception){
-            throw new RuntimeException("JWT Failed to Create.");
-        }
-        return token;
+    private void validatePassword() {
+        mPassWordValidator.processResult(
+                mPassWordValidator.apply(binding.editTextPassword.getText().toString()),
+                this::verifyAuthWithServer,
+                result -> binding.editTextPassword.setError("Please enter a valid Password."));
     }
+
+
+
+
 
     private void verifyAuthWithServer() {
         mSignInModel.connect(
                 binding.editTextEmail.getText().toString(),
                 binding.editTextPassword.getText().toString());
-
-        //This is an Asynchronous call. No statements after should rely on the result of connect.
+        //This is an Asynchronous call. No statements after should rely on the
+        // result of connect().
 
     }
 
@@ -132,9 +125,7 @@ public class LogInFragment extends Fragment {
     private void navigateToSuccess(final String email, final String jwt) {
         Navigation.findNavController(getView())
                 .navigate(LogInFragmentDirections
-                        .actionLogInFragmentToMainActivity(jwt, email));
-
-        getActivity().finish();
+                        .actionLogInFragmentToMainActivity(email, jwt));
     }
 
     /**
@@ -144,22 +135,19 @@ public class LogInFragment extends Fragment {
      * @param response the Response from the server
      */
     private void observeResponse(final JSONObject response) {
+
         if (response.length() > 0) {
             if (response.has("code")) {
                 try {
                     binding.editTextEmail.setError(
-                            "Error Authenticating: " +
-                                    response.getJSONObject("data").getString("message"));
+                            "Error Authenticating: " + response.getJSONObject("data").getString("message"));
                 } catch (JSONException e) {
                     Log.e("JSON Parse Error", e.getMessage());
                 }
             } else {
-
                 try {
-
                     navigateToSuccess(
-                            binding.editTextEmail.getText().toString(), response.getString("token")
-                    );
+                            binding.editTextEmail.getText().toString(), response.getString("token") );
                 } catch (JSONException e) {
                     Log.e("JSON Parse Error", e.getMessage());
                 }
@@ -167,7 +155,5 @@ public class LogInFragment extends Fragment {
         } else {
             Log.d("JSON Response", "No Response");
         }
-
     }
-
 }
