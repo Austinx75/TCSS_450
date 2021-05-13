12d8a2de177842495914c0805cc2cc6cad3cfbe8
Austin Scott <austin.l.scott75@gmail.com>
2021-05-12 19:06:12 -0700

    Color and Toolbar w/ Settings fragment

--- Harmony/app/src/main/java/edu/uw/harmony/MainActivity.java
+++ Harmony/app/src/main/java/edu/uw/harmony/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -36,5 +39,19 @@ public class MainActivity extends AppCompatActivity {
             NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
             return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                     || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    //To handle clicks
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item);
     }
 }
