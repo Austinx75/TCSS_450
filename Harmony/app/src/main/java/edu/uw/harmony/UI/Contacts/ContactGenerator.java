629aec1a3c94fa961cfb8752add5cf82e87cda3b
Hermie Baylon <hermib100@gmail.com>
2021-05-20 19:16:49 -0700

    documentation for contacts

--- Harmony/app/src/main/java/edu/uw/harmony/UI/Contacts/ContactGenerator.java
+++ Harmony/app/src/main/java/edu/uw/harmony/UI/Contacts/ContactGenerator.java
@@ -15,8 +15,14 @@ import edu.uw.harmony.R;
  */
 
 public class ContactGenerator {
+
+    /** array of contacts */
     private static final ContactCard[] CONTACTS;
+
+    /** the number of contacts */
     public static final int COUNT = 20;
+
+    /** the array of names to assign the contacts for now */
     public static String[] names = {"Tilly Britt", "Damien Church", "Mia Ali", "Ayesha Garcia", "Iga Montes",
                                     "Maddie Donaldson", "Humera Mccartney", "Marnie Bouvet", "Zane Shannon",
                                     "Marlon Cantu", "Nayan Zuniga", "Kerry Weiss", "Tyson Pittman", "Carolyn Lake",
@@ -27,7 +33,11 @@ public class ContactGenerator {
                                         "Keblex", "Goliread", "Flower Yoda", "TheLionKing", "BlackHoleSnail",
                                         "RhythmStar", "Fox", "IHeartCatnip", "AbleCheese", "RatSauce", "ThePrestige",
                                         "BatmanBegins"};
+
+    /** the different types of status a contact can have */
     public static String[] status = {"Online", "Away", "Idle"};
+
+    /** array of images used for contact avatars */
     public static int[] images = {R.drawable.contact_boy_512, R.drawable.contact_hacker_512,R.drawable.contact_barista_512,
             R.drawable.contact_kitty_512,R.drawable.contact_man_512,R.drawable.contact_man_1_512,
             R.drawable.contact_man_2_512,R.drawable.contact_user_512,R.drawable.contact_woman_512,
@@ -44,13 +54,23 @@ public class ContactGenerator {
         }
     }
 
+    /**
+     * Getter method for contact list
+     * @return contact list
+     */
     public static List<ContactCard> getContactList() {
         return Arrays.asList(CONTACTS);
     }
 
+    /**
+     * @return  a copy of the contact list
+     */
     public static ContactCard[] getCONTACTS() {
         return Arrays.copyOf(CONTACTS, CONTACTS.length);
     }
 
+    /**
+     * private constructor
+     */
     private ContactGenerator() { }
 }
