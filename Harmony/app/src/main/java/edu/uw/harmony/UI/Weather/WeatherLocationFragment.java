1950a6aa44e8b6147ba02cfe135afbe08a63d43f
Gary Kono <gary.a.kono@gmail.com>
2021-05-30 14:23:34 -0700

    fixed weather tab bugs, setup zip code handling, added tabs and map to weather

---
+++ Harmony/app/src/main/java/edu/uw/harmony/UI/Weather/WeatherLocationFragment.java
@@ -0,0 +1,112 @@
+package edu.uw.harmony.UI.Weather;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import edu.uw.harmony.R;
+import edu.uw.harmony.UI.model.LocationViewModel;
+import edu.uw.harmony.databinding.FragmentWeatherLocationBinding;
+
+/**
+ *
+ *
+ *
+ * @author  Gary Kono
+ * @version 1.2
+ */
+public class WeatherLocationFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    private LocationViewModel mModel;
+    private GoogleMap mMap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_location, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FragmentWeatherLocationBinding binding = FragmentWeatherLocationBinding.bind(getView());
+
+        mModel = new ViewModelProvider(getActivity())
+                .get(LocationViewModel.class);
+        mModel.addLocationObserver(getViewLifecycleOwner(),
+                location ->
+                        binding.textLatLong.setText(location.toString()));
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        //add this fragment as the OnMapReadyCallback -> See onMapReady()
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LocationViewModel model = new ViewModelProvider(getActivity())
+                .get(LocationViewModel.class);
+        model.addLocationObserver(getViewLifecycleOwner(), location -> {
+            if (location != null) {
+                googleMap.getUiSettings().setZoomControlsEnabled(true);
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+
+                final LatLng c = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                //Zoom levels are from 2.0f (zoomed out) to 21.f (zoomed in)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(c, 15.0f));
+            }
+        });
+
+        mMap.setOnMapClickListener(this);
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.d("LAT/LONG", latLng.toString());
+
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("New Marker"));
+
+        mMap.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                        latLng, mMap.getCameraPosition().zoom));
+    }
+}
